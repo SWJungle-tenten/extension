ab8f25fee763be809158b6edcda6eb22a9713263
terry <kdjtiger@gmail.com>
2023-06-10 20:04:32 +0900

    init homepage

--- public/contentScript.js
+++
@@ -1,6 +0,0 @@
-const originBanner = document.getElementById("sub_pack");
-
-originBanner.insertAdjacentHTML(
-  "afterend",
-  `<div><button><a href="https://naver.com">버튼을 눌러보세요</a></button><div>히히</div></div>`
-);
