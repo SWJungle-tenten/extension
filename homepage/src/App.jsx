278047df05e77043c7cbb8eeaf8ad9285920aacb
SungBeum <an4126@gmail.com>
2023-06-15 01:30:48 +0900

    Fix: conflict complete

--- homepage/src/App.jsx
+++ homepage/src/App.jsx
@@ -2,12 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { CookiesProvider } from "react-cookie";
 import Intro from "./components/Intro/Intro";
 import Main from "./components/Main/Main";
-// import Storage from "./components/Storage/Storage";
-// import StorageHome from "./components/Storage/StorageHome";
-import StoragePosts from "./components/Storage/StorageHome";
-import SubComponent from "./components/Storage/StorageHome";
-import MyComponent from "./components/Storage/MyComponent";
-// import StoragePosts from "./components/Storage/StoragePosts";
+import Storage from "./components/Storage/Storage";
 
 
 export default function App() {
@@ -18,7 +13,7 @@ export default function App() {
           <Route path="/" element={<Intro />} />
           <Route path="/main" element={<Main />} />
           <Route path="/storage/*" element={<Storage />} />
-          <Route path="*" element={<NotFound />} />
+          <Route path="*" element={<Main />} />
         </Routes>
       </BrowserRouter>
     </CookiesProvider>
