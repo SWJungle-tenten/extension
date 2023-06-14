ed569b6b56f7a7bb626d2581697295e0d230addf
황채림 <cofla159@naver.com>
2023-06-15 04:09:20 +0900

    fix: 홈페이지 로그인/로그아웃될 때마다 팝업 화면 업데이트

--- extension/popup.js
+++ extension/popup.js
@@ -1,24 +1,37 @@
 const HOMEPAGE_ADDR = "http://localhost:3000";
 
-let accessToken;
-chrome.runtime.sendMessage({ action: "getVariable" }, function (response) {
-  accessToken = response.accessToken;
+let innerMessage = "스크랩하려면 로그인해주세요.";
+let buttonLink = HOMEPAGE_ADDR;
+let buttonText = "로그인 하러가기";
 
-  let innerMessage, buttonLink, buttonText;
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.action === "updateToken") {
+    if (message.token) {
+      innerMessage = "환영합니다 ㅇㅇㅇ님!";
+      buttonLink = HOMEPAGE_ADDR + "/storage";
+      buttonText = "보관함 바로가기";
+    } else {
+      innerMessage = "스크랩하려면 로그인해주세요.";
+      buttonLink = HOMEPAGE_ADDR;
+      buttonText = "로그인 하러가기";
+    }
+  }
+  document.querySelector("#welcome-message").innerText = innerMessage;
+  document.querySelector("#link-btn").innerText = buttonText;
+});
 
-  if (accessToken) {
+chrome.runtime.sendMessage({ action: "getToken" }, (response) => {
+  if (response.accessToken) {
     innerMessage = "환영합니다 ㅇㅇㅇ님!";
     buttonLink = HOMEPAGE_ADDR;
     buttonText = "보관함 가기";
-  } else {
-    innerMessage = "스크랩하려면 로그인해주세요.";
-    buttonLink = HOMEPAGE_ADDR;
-    buttonText = "로그인 하러가기";
   }
-
   document.querySelector("#welcome-message").innerText = innerMessage;
-  document.querySelector("#link-btn").addEventListener("click", function () {
-    chrome.tabs.create({ url: HOMEPAGE_ADDR });
-  });
   document.querySelector("#link-btn").innerText = buttonText;
+  document.querySelector("#link-btn").addEventListener("click", function () {
+    chrome.tabs.create({ url: buttonLink });
+  });
 });
+
+
+
--- extension/service-worker.js
+++ extension/service-worker.js
@@ -1,11 +1,18 @@
 const SERVER_ADDR = "http://localhost:8080";
 
-const accessToken = (async () => {
-  return await chrome.cookies.get({ url: SERVER_ADDR, name: "accessToken" }).value;
-})();
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.action === "getToken") {
+    chrome.cookies.get({ url: SERVER_ADDR, name: "accessToken" }).then((response) => {
+      sendResponse({ accessToken: response?.value });
+    });
+  }
+  return true;
+});
 
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-  if (message.action === "getVariable") {
-    sendResponse({ accessToken });
+chrome.cookies.onChanged.addListener(({ cause, cookie, removed }) => {
+  if (cookie.name === "accessToken") {
+    chrome.cookies.get({ url: SERVER_ADDR, name: "accessToken" }).then((response) => {
+      chrome.runtime.sendMessage({ action: "updateToken", token: response?.value });
+    });
   }
 });
