5e29956cc7592cb69da95d72853bc7d31a300895
SungBeum <an4126@gmail.com>
2023-06-15 22:45:29 +0900

    feat: Sidebar API connection complete

--- homepage/src/components/storage/Sidebar.jsx
+++ homepage/src/components/storage/Sidebar.jsx
@@ -1,12 +1,41 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
+import { useCookies } from "react-cookie";
 
-export default function Sidebar( userData ) {
-  console.log('user',userData);
+export default function Sidebar() {
+  const [cookies] = useCookies(["accessToken"]);
+  const [userName, setUserName] = useState(null);
+
+  useEffect(() => {
+    const fetchUserName = async () => {
+      console.log('cookies.accessToken', cookies.accessToken);
+      try {
+        const response = await fetch(`${process.env.REACT_APP_SERVER_ADDR}/api/giveUserName`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ userToken: cookies.accessToken }),
+        });
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        console.log('data', data);
+        setUserName(data.username);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchUserName();
+  }, [cookies.accessToken]);
+
   return (
     <div>
-      <p className="text-lg font-bold">Sidebar</p>
-      <p>{userData?.nickName}</p>
+      <p className="text-3xl font-bold">Sidebar</p>
+      <p className="text-2xl">반가워요 {userName} 님 !!!</p>
     </div>
   );
 };
-
