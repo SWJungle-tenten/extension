7787156f514c76b11a33406404d98842a332fe6a
황채림 <cofla159@naver.com>
2023-06-18 01:26:26 +0900

    fix: 상단에 넓은 바가 있을 때의 preview 창 위치 예외처리

--- reactContentScripts/src/index.tsx
+++ reactContentScripts/src/index.tsx
@@ -17,8 +17,20 @@ const changeDomainComponent = () => {
     return <Naver />;
   } else if (curOrigin + curPathname === "https://www.google.com/search") {
     const sidebar = document.querySelector(".TQc1id");
+    const topSnippet = document.querySelector(".M8OgIe");
     if (sidebar) {
       sidebar.appendChild(rootElement);
+    } else if (topSnippet) {
+      const wrapper = document.createElement("div");
+      wrapper.className = "content-warpper";
+      wrapper.style.display = "flex";
+      wrapper.style.flexDirection = "row";
+
+      const originLeft = document.querySelector("#center_col");
+      wrapper.appendChild(originLeft);
+      wrapper.appendChild(rootElement);
+      // document.querySelector(".GyAeWb").replaceChild(wrapper, originLeft);
+      document.querySelector(".GyAeWb").appendChild(wrapper);
     } else {
       const main = document.querySelector(".GyAeWb");
       main.append(rootElement);
