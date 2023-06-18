307d4c811727820f7aa3fc8added7191ca10d7ab
황채림 <cofla159@naver.com>
2023-06-19 00:19:31 +0900

    feat: previewer가 오른쪽 사이드바 위로 렌더링

--- reactContentScripts/src/index.tsx
+++ reactContentScripts/src/index.tsx
@@ -16,25 +16,26 @@ const changeDomainComponent = () => {
     document.querySelector("#sub_pack").append(rootElement);
     return <Naver />;
   } else if (curOrigin + curPathname === "https://www.google.com/search") {
-    const sidebar = document.querySelector(".TQc1id");
+    const body = document.querySelector(".GyAeWb");
+    const rightSide = document.querySelector(".TQc1id");
     const topSnippet = document.querySelector(".M8OgIe");
-    if (sidebar) {
-      sidebar.appendChild(rootElement);
+    if (rightSide) {
+      body.removeChild(rightSide);
+      body.appendChild(rootElement);
+      body.style.flexWrap = "nowrap";
     } else if (topSnippet) {
       const wrapper = document.createElement("div");
       wrapper.className = "content-warpper";
       wrapper.style.display = "flex";
       wrapper.style.flexDirection = "row";
 
-      const originLeft = document.querySelector("#center_col");
-      wrapper.appendChild(originLeft);
+      const leftSide = document.querySelector("#center_col");
+      wrapper.appendChild(leftSide);
       wrapper.appendChild(rootElement);
-      // document.querySelector(".GyAeWb").replaceChild(wrapper, originLeft);
-      document.querySelector(".GyAeWb").appendChild(wrapper);
+      body.appendChild(wrapper);
     } else {
-      const main = document.querySelector(".GyAeWb");
-      main.append(rootElement);
-      main.style.flexWrap = "nowrap";
+      body.append(rootElement);
+      body.style.flexWrap = "nowrap";
     }
     return <Google />;
   } else {
