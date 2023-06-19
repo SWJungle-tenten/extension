c51f0377a4b12f66417a6010ad14ececffc1ae64
황채림 <cofla159@naver.com>
2023-06-19 14:48:21 +0900

    feat: 유저 이름 보여주기

--- extension/popup.js
+++ extension/popup.js
@@ -4,13 +4,17 @@ let innerMessage = "스크랩하려면 로그인해주세요.";
 let buttonLink = HOMEPAGE_ADDR;
 let buttonText = "로그인 하러가기";
 
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+let userName;
+
+chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
   if (message.action === "updateToken") {
     if (message.accessToken) {
-      innerMessage = "환영합니다 ㅇㅇㅇ님!";
+      userName = await getUserName(message.accessToken);
+      innerMessage = `환영합니다 ${userName}님!`;
       buttonLink = HOMEPAGE_ADDR + "/storage";
       buttonText = "보관함 바로가기";
     } else {
+      userName = "";
       innerMessage = "스크랩하려면 로그인해주세요.";
       buttonLink = HOMEPAGE_ADDR;
       buttonText = "로그인 하러가기";
@@ -20,11 +24,17 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   document.querySelector("#link-btn").innerText = buttonText;
 });
 
-chrome.runtime.sendMessage({ action: "getToken" }, (response) => {
+chrome.runtime.sendMessage({ action: "getToken" }, async (response) => {
   if (response.accessToken) {
-    innerMessage = "환영합니다 ㅇㅇㅇ님!";
+    userName = await getUserName(response.accessToken);
+    innerMessage = `환영합니다 ${userName}님!`;
     buttonLink = HOMEPAGE_ADDR + "/storage";
     buttonText = "보관함 가기";
+  } else {
+    userName = "";
+    innerMessage = "스크랩하려면 로그인해주세요.";
+    buttonLink = HOMEPAGE_ADDR;
+    buttonText = "로그인 하러가기";
   }
   document.querySelector("#welcome-message").innerText = innerMessage;
   document.querySelector("#link-btn").innerText = buttonText;
@@ -33,5 +43,18 @@ chrome.runtime.sendMessage({ action: "getToken" }, (response) => {
   });
 });
 
-
+const getUserName = async (accessToken) => {
+  try {
+    const response = await fetch(`http://localhost:8080/api/giveUserName`, {
+      method: "post",
+      headers: new Headers({
+        Authorization: `Bearer ${accessToken}`,
+      }),
+    });
+    const data = await response.json();
+    return data.username;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
