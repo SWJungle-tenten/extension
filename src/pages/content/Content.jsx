a228babbedc317b3d127cb4a287e369bff16997e
SungBeum <an4126@gmail.com>
2023-06-21 22:39:01 +0900

    Feat: SubScrap in the parentDocument

--- src/pages/content/Content.jsx
+++ src/pages/content/Content.jsx
@@ -1,8 +1,10 @@
-// @ts-nocheck
 import React, { useState, useEffect } from "react";
 import ScrapButton from "./components/ScrapButton";
+import axios from "axios";
 
 function Content() {
+  const [scrapButton, setScrapButton] = useState(null);
+  const [selectedText, setSelectedText] = useState(null);
   const [previewUrl, setPreviewUrl] = useState(null);
   const [accessToken, setAccessToken] = useState(null);
 
@@ -16,7 +18,8 @@ function Content() {
           setPreviewUrl(eventTarget.parentElement.href);
         } else if (
           (eventTarget.tagName === "A") &
-          (((eventTarget.className === "") & (eventTarget.id === "")) | (eventTarget.className === "l"))
+          (((eventTarget.className === "") & (eventTarget.id === "")) |
+            (eventTarget.className === "l"))
         ) {
           setPreviewUrl(eventTarget.href);
         }
@@ -24,6 +27,7 @@ function Content() {
     },
     { capture: true }
   );
+
   useEffect(() => {
     chrome.runtime.sendMessage({ action: "getToken" }, (response) => {
       if (response.accessToken) {
@@ -38,6 +42,87 @@ function Content() {
     }
   });
 
+  useEffect(() => {
+    const mouseDownHandler = function (e) {
+      if (!scrapButton) {
+        return;
+      }
+      if (e.target !== scrapButton || selectedText === null) {
+        document.body.removeChild(scrapButton);
+        setScrapButton(null);
+      }
+    };
+    
+    document.addEventListener("mousedown", mouseDownHandler);
+    return () => {
+      document.removeEventListener("mousedown", mouseDownHandler);
+    };
+  }, [scrapButton, selectedText]);
+
+  useEffect(() => {
+    const mouseUpHandler = async function (e) {
+      if (e.target === scrapButton || e.target === selectedText) {
+        return;
+      }
+      const sel = document.getSelection();
+      if (
+        sel.isCollapsed ||
+        (sel.toString().length < 6 && sel.toString().includes("\n")) ||
+        sel.toString() === selectedText
+      ) {
+        return;
+      } else {
+        const direction = sel.anchorOffset - sel.focusOffset < 0;
+        const divTop = direction ? e.pageY + 10 : e.pageY - 40;
+        const divLeft = direction ? e.pageX + 10 : e.pageX - 40;
+
+        const button = document.createElement("button");
+        button.innerHTML = "Sub Scrap";
+        button.style.position = "absolute";
+        button.style.top = `${divTop}px`;
+        button.style.left = `${divLeft}px`;
+        document.body.appendChild(button);
+        setScrapButton(button);
+        setSelectedText(sel.toString());
+
+        button.addEventListener("click", async function () {
+          const data = {
+            keyWord: document.querySelector("#APjFqb").innerHTML,
+            url: window.location.href,
+            title: document.title,
+            texts: [sel.toString()],
+          };
+
+          try {
+            const response = await axios.post(
+              "http://localhost:8080/api/saveScrapText",
+              { data },
+              { headers: { Authorization: `Bearer ${accessToken}` } }
+            );
+
+            if (response.status === 200) {
+              console.log("response", response)
+              button.innerHTML = "Sub Scrap Success";
+              button.disabled = true;
+              setTimeout(() => {
+                button.style.display = "none";
+              }, 1000);
+            } else {
+              alert("Sub Scrap Failed");
+            }
+          } catch (error) {
+            alert("Sub Scrap Failed");
+          }
+        });
+      }
+    };
+
+    document.addEventListener("mouseup", mouseUpHandler);
+    return () => {
+      document.removeEventListener("mouseup", mouseUpHandler);
+    };
+  }, [scrapButton, selectedText, accessToken]);
+
   return (
     <>
       {accessToken && <ScrapButton accessToken={accessToken} />}
@@ -51,7 +136,12 @@ function Content() {
           position: "sticky",
         }}
       >
-        <iframe id="previewer" title="previewer" src={previewUrl} style={{ width: "100%", height: "100%" }}></iframe>
+        <iframe
+          id="previewer"
+          title="previewer"
+          src={previewUrl}
+          style={{ width: "100%", height: "100%" }}
+        ></iframe>
       </div>
     </>
   );
