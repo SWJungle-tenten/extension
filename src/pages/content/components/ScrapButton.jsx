c149db8944bd9fb29c1f6b11095de54d8fe80ddc
황채림 <cofla159@naver.com>
2023-06-30 16:07:57 +0900

    chore: 콘솔 및 주석 삭제

--- src/pages/content/components/ScrapButton.jsx
+++ src/pages/content/components/ScrapButton.jsx
@@ -5,7 +5,6 @@ import alertSweetBeum from "../utils/alertSweetBeum";
 
 function ScrapButton({ accessToken }) {
   const scrapHandler = (event) => {
-    // event.stopPropagation();
     event.preventDefault();
 
     const iframe = document.querySelector("#previewer");
@@ -24,13 +23,11 @@ function ScrapButton({ accessToken }) {
       data,
     })
       .then((response) => {
-        console.log(response);
-        // alert("스크랩 성공");
-        alertSweetBeum("성공","링크");
+        alertSweetBeum("성공", "링크");
       })
       .catch((error) => {
-        // alert("스크랩 실패", error);
-        alertSweetBeum("실패","링크");
+        alertSweetBeum("실패", "링크");
+        console.error(error);
       });
   };
   return (
