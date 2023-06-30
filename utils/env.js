a2494983da18da46790a109505e9be99b489ddaa
황채림 <cofla159@naver.com>
2023-06-30 21:18:14 +0900

    fix: dev 버전 빌드되지 않는 현상

--- utils/env.js
+++ utils/env.js
@@ -1,6 +1,6 @@
 module.exports = {
   NODE_ENV: process.env.NODE_ENV || "development",
   PORT: 5500,
-  HOMEPAGE_ADDR: this.NODE_ENV === "development" ? "http://localhost:3000" : "https://googlinghelper.shop",
-  SERVER_ADDR: this.NODE_ENV === "development" ? "http://localhost:8080" : "https://sangunlee.shop",
+  HOMEPAGE_ADDR: process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://googlinghelper.shop",
+  SERVER_ADDR: process.env.NODE_ENV === "development" ? "http://localhost:8080" : "https://sangunlee.shop",
 };
