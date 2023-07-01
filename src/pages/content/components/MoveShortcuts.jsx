e85464af6791292acb292216f760da7ab143204a
황채림 <cofla159@naver.com>
2023-07-02 04:03:55 +0900

    refactor: 키보드 단축키 비인물 최상위/최하위 이동 구현 - 인물 검색과 비인물 검색 분기 - 최상위/최하위 외에는 아직 동작 안함

--- src/pages/content/components/MoveShortcuts.jsx
+++ src/pages/content/components/MoveShortcuts.jsx
@@ -11,34 +11,56 @@ function MoveShortcuts({ setPreviewUrl, setPreviewTitle, previousContainer }) {
         e.preventDefault();
 
         const curPreview = document.querySelector("#previewer").src;
-        if (!curPreview) {
-          if (e.code === "KeyS") {
-            document.querySelector(".MjjYud:first-child").querySelector("a").focus();
+        const searchContainer = document.querySelector("#rso");
+        let nextFocus;
+        // 인물 검색
+        if (searchContainer.querySelector("#kp-wp-tab-overview")) {
+          if (!curPreview) {
+            if (e.code === "KeyS") {
+              searchContainer.querySelector("#kp-wp-tab-overview > .cLjAic:first-child").querySelector("a").focus();
+            } else {
+              // searchContainer.querySelector("#kp-wp-tab-overview > .cLjAic:last-child").querySelector("a").focus();
+            }
           } else {
-            document.querySelector(".MjjYud:last-child").querySelector("a").focus();
+            // previewer에 있을 때
           }
         } else {
-          const curFocus = document.querySelector(`a[href="${curPreview}"]`);
-
-          const topLevelContainer = curFocus.closest(".hlcw0c")
-            ? curFocus.closest(".hlcw0c")
-            : curFocus.closest(".MjjYud");
-
-          let nextFocus = topLevelContainer;
-          if (e.code === "KeyS") {
-            while (nextFocus.nextElementSibling) {
-              nextFocus = nextFocus.nextElementSibling;
-              if (nextFocus.querySelector("a")) {
-                break;
-              }
+          // 인물검색 외
+          if (!curPreview) {
+            if (e.code === "KeyS") {
+              searchContainer.querySelector(".v7W49e > div:first-child").querySelector("a").focus(); // 마우스랑 setAttribute를 따로 쓰면 작은 a태그도 처리 가능할 듯?
+            } else {
+              searchContainer.querySelector(".v7W49e > div:last-child").querySelector("a").focus();
             }
           } else {
-            while (nextFocus.previousElementSibling) {
-              nextFocus = nextFocus.previousElementSibling;
-              if (nextFocus.querySelector("a")) break;
-            }
+            const curFocus = document.querySelector(`a[href="${curPreview}"]`);
+
+            const topLevelContainer = curFocus.closest(".hlcw0c")
+              ? curFocus.closest(".hlcw0c")
+                ? curFocus.closest(".ULSxyf")
+                : curFocus.closest(".ULSxyf")
+              : curFocus.closest(".MjjYud");
+
+            console.log(topLevelContainer);
+
+              // let nextFocus = topLevelContainer;
+              if (e.code === "KeyS") {
+                topLevelContainer.nextElementSibling.querySelector("a").focus();
+                // while (nextFocus.nextElementSibling) {
+                //   nextFocus = nextFocus.nextElementSibling;
+                //   if (nextFocus.querySelector("a")) {
+                //     break;
+                //   }
+                // }
+              } else {
+                topLevelContainer.previousElementSibling.querySelector("a").focus();
+                // while (nextFocus.previousElementSibling) {
+                //   nextFocus = nextFocus.previousElementSibling;
+                //   if (nextFocus.querySelector("a")) break;
+                // }
+              }
+            // nextFocus.querySelector("a")?.focus();
           }
-          nextFocus.querySelector("a")?.focus();
         }
       }
     };
@@ -46,9 +68,9 @@ function MoveShortcuts({ setPreviewUrl, setPreviewTitle, previousContainer }) {
     document.addEventListener("keypress", handleKeyPress);
 
     const handleFocus = async (e) => {
+      moveFocusBox(previousContainer, e.target.parentElement, true);
       const [url, title] = await setPreviewAttributes(e, 500, "keyboard");
       if (url && title) {
-        moveFocusBox(previousContainer, e.target.parentElement, true);
         setPreviewUrl(url);
         setPreviewTitle(title);
       }
